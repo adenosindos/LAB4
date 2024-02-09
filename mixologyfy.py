deca7842b5bd8d764b6f82eb9741d34d54400798
adenosindos <158822449+adenosindos@users.noreply.github.com>
2024-02-09 10:20:50 +0100

    Update mixologyfy.py

--- mixologyfy.py
+++ mixologyfy.py
@@ -27,7 +27,7 @@ http://raw.githubusercontent.com/teijo/iba-cocktails/master/recipes.json
 "APP_TEMPLATE_PATH":
 File system path to Jinja2 HTML template for web application.
 Default:
-"/opt/mixologyfy/index.html.jinja"
+"/home/ubuntu/index.html.jinja"
 
 "APP_DATABASE_URI":
 PostgreSQL connection URI required for storing favorites (and "VG" grade).
